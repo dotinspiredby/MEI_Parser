92d9ae95a7c24ffb32128405b6afdd19e322a64b
dotinspiredby <81487419+dotinspiredby@users.noreply.github.com>
2021-06-17 00:25:26 +0300

    Add files via upload

---
+++ mei_conv.py
@@ -0,0 +1,25 @@
+import requests
+from music21 import mei
+import sys
+
+
+class ParserConverter:
+
+    def __init__(self, link):
+        self.score = self.__parse_link(link)
+
+    @staticmethod
+    def __parse_link(url):
+        parsed = requests.get(url)
+        to_conv = mei.MeiToM21Converter(parsed.text)
+        stream_obj = to_conv.run()
+        return stream_obj
+
+    def run(self):
+        self.score.show()
+
+
+if __name__ == "__main__":
+    new = ParserConverter(sys.argv[1])
+    new.run()
+http://digitalduchemin.org/mei/DC1413.xml
